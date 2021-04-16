cea763d321438cfabb9ba1d7ff6dbfea87175494
shrutipanchal7699 <shruti.panchal@sjsu.edu>
2021-04-16 15:56:08 -0700

    added the PORT for app.list

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import data from './data';
+import data from './data.js';
 
 
 const app = express();
 
 app.get("/api/products", (req,res)=> {
     res.send(data.products);
-})
+});
+app.get('/',(req,res) => {
+    res.send('Server is ready!');
+});
 
 app.listen(5000, ()=>{console.log("Server started at http://localhost:5000")})
