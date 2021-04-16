c9025791fe826b54f02b99ba61c3374fabea37bd
shrutipanchal7699 <shruti.panchal@sjsu.edu>
2021-04-16 16:28:35 -0700

    using constant port

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,6 @@ app.get('/',(req,res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{console.log("Server started at http://localhost:${port}");
+app.listen(port, ()=>{
+    console.log(`Server started at http://localhost:${port}`);
 });
