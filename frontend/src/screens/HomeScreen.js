ad1f2d4098045cbae3f5912a23f6e3b674229b35
shrutipanchal7699 <shruti.panchal@sjsu.edu>
2021-04-17 21:11:35 -0700

    added the comments for HomeScreen.js file

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,18 +3,28 @@ import axios from 'axios';
 import Product from '../components/Product';
 
 export default function HomeScreen() {
+    // defining the hooks
     const [products, setProducts] = useState([]);
+    
+    // runs only one time, sending an ajax request
     useEffect(() => {
         const fetchData = async () =>{
+            //fetching data from axios 
+            // array will be transformed from the data in backedn to the data in frontend
             const { data } = await axios.get('/api/products');
+            
+            //setting the products to the data we are getting from backend.
             setProducts(data);
         };
+
+        //calling the already defined fetchdata here.
         fetchData();
     }, [])
     return(
         <div>
             
         <div className="row center">
+                    {/* using products from backend */}
                     {products.map(product => (
                             <Product key={product._id} product={product}></Product>
                               
