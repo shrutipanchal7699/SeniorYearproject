ca3c1798dc9d6fbe091c136432b297bd1a365f2c
shrutipanchal7699 <shruti.panchal@sjsu.edu>
2021-04-17 22:18:36 -0700

    added Loaded and MessageBox

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,39 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Product from '../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
 
 export default function HomeScreen() {
     // defining the hooks
     const [products, setProducts] = useState([]);
+
+    //default setting is false, as it doesn't load anything
+    const [loading, setLoading] = useState(false);
+
+    const [error, setError] = useState(false);
     
     // runs only one time, sending an ajax request
     useEffect(() => {
         const fetchData = async () =>{
-            //fetching data from axios 
-            // array will be transformed from the data in backedn to the data in frontend
-            const { data } = await axios.get('/api/products');
+
+            try{
+                setLoading(true);
             
-            //setting the products to the data we are getting from backend.
-            setProducts(data);
+                //fetching data from axios 
+                // array will be transformed from the data in backedn to the data in frontend
+                const { data } = await axios.get('/api/products');
+                setLoading(false);
+    
+                //setting the products to the data we are getting from backend.
+                setProducts(data);
+            }catch(err){
+                setError(err.message);
+                setLoading(false);
+            }
+            
+            
         };
 
         //calling the already defined fetchdata here.
@@ -22,16 +41,19 @@ export default function HomeScreen() {
     }, [])
     return(
         <div>
-            
-        <div className="row center">
+            { loading ? (
+                <LoadingBox></LoadingBox>
+            ) : error? (
+                <MessageBox variant="danger">{error}</MessageBox>
+            ) : (
+                    <div className="row center">
                     {/* using products from backend */}
                     {products.map(product => (
                             <Product key={product._id} product={product}></Product>
                               
-                        ))
-                    }                    
-                </div>
-                
-        </div>
+                ))}                    
+            </div>
+        )}       
+    </div>
     );
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Product from '../components/Product.js';
+// import Product from '../components/Product.js';
 import data from '../data';
 
 
@@ -48,7 +48,7 @@ export default function ProductScreen(props) {
                                         {product.countInStock > 0 ? (
                                             <span className="success"> In Stock</span>
                                         ) : (
-                                            <span className="error">Unavailable</span>
+                                            <span className="danger">Unavailable</span>
                                         )}
                                     </div>
                                 </div>
