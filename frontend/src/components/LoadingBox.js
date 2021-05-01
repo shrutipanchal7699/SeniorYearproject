35a05882b0956b3f3b6cfe9a84abeffbb4dfd13c
shrutipanchal7699 <shruti.panchal@sjsu.edu>
2021-04-30 19:45:54 -0700

    added the sign up screen

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 // Sending a loading message to AJAX server before displaying the products
 export default function LoadingBox(){
     return (
-        <div>
+        <div className ="loading">
 
            {/* defining a simple loading component  */}
            <i className="fa fa-spinner fa-spin">Wait... Loading...</i>
